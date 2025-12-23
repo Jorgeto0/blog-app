926fb9a558e79244f9790e408cf7bfe42405a3c7
jorge <jorge.girgis28@gmail.com>
2025-12-23 17:17:34 +0200

    added a me API test

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->get('/me', function () {
+    return auth()->user();
+});
